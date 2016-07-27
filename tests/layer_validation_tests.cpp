10cf1627578470166abc6c43760611312dbee2ad
Cort <github@dangerware.org>
2016-07-27 09:51:27 -0700

    tests: Fixed typo in test name
    DynamiBlendConstantsNotBound -> DynamicBlendConstantsNotBound
    
    Change-Id: If30b93d8cfb32dade686d44b615b772f6504ee17

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -6396,7 +6396,7 @@ TEST_F(VkLayerTest, DynamicScissorNotBound) {
     m_errorMonitor->VerifyFound();
 }
 
-TEST_F(VkLayerTest, DynamiBlendConstantsNotBound) {
+TEST_F(VkLayerTest, DynamicBlendConstantsNotBound) {
     TEST_DESCRIPTION(
         "Run a simple draw calls to validate failure when Blend Constants "
         "dynamic state is required but not correctly bound.");
